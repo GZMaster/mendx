5b90e8f169aec45d5b4014491ef03a4f31c044ad
GZMaster <danigamester@gmail.com>
2022-10-22 20:08:07 +0100

    about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import UseMediaQuery from "../../components/mediaquery/UseMediaQuerry";
 import "./About.scss";
-import AboutImage1 from "../../assets/images/About Us Image 1Mendx.png";
-import AboutImage1Mobile from "../../assets/images/About Us Image 1mobile.png";
-import AboutImage2 from "../../assets/images/About Us Image 2Mendx.png";
-import AboutImage2Mobile from "../../assets/images/About Us Image 2Mobile.png";
-import Flag from "../../assets/images/Nigerian Map.png";
-import FlagMobile from "../../assets/images/Nigerian Mapmobile.png";
+import AboutImage from "../../assets/images/Images Plus Map_About.png";
+import AboutImageMobile from "../../assets/images/Images plus map Mobile_About_Mobile.png";
 
 const About = () => {
   let isPageWide = UseMediaQuery("(max-width: 468px)");
@@ -17,15 +13,11 @@ const About = () => {
         <div className="About_Image_Wrap">
           {!isPageWide ? (
             <div className="About_Image">
-              <img src={Flag} className="Image_Flag" alt="" />
-              {/* <img src={AboutImage1} className="Image_1" alt="" /> */}
-              {/* <img src={AboutImage2} className="Image_2" alt="" /> */}
+              <img src={AboutImage} className="Image" alt="" />
             </div>
           ) : (
             <div className="About_Image">
-              <img src={FlagMobile} className="Image_Flag" alt="" />
-              <img src={AboutImage1Mobile} className="Image_1" alt="" />
-              <img src={AboutImage2Mobile} className="Image_2" alt="" />
+              <img src={AboutImageMobile} className="Image" alt="" />
             </div>
           ) }
         </div>
