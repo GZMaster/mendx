ed9b2cd063ac79b80fa73a70b8d3acd23f2c8e11
GZMaster <danigamester@gmail.com>
2022-10-22 22:45:00 +0100

    about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -3,6 +3,9 @@ import UseMediaQuery from "../../components/mediaquery/UseMediaQuerry";
 import "./About.scss";
 import AboutImage from "../../assets/images/Images Plus Map_About.png";
 import AboutImageMobile from "../../assets/images/Images plus map Mobile_About_Mobile.png";
+import icon1 from "../../assets/images/Thumbs up_AboutIcon.png";
+import icon2 from "../../assets/images/Eye_AboutIcon.png";
+import icon3 from "../../assets/images/Badge_AboutIcon.png";
 
 const About = () => {
   let isPageWide = UseMediaQuery("(max-width: 468px)");
@@ -22,26 +25,37 @@ const About = () => {
           ) }
         </div>
         <div className="About_Text_Wrapper">
-          <p>Est vitae vitae sagittis tellus vitae.</p>
+          <div className="About_Text_Heading">
+            <p>Get to know us more</p>
+            <h3 className="About_Text_Header">Benefits of <span>Partnering with</span> us.</h3>
+          </div>
 
-          <h3 className="About_Text_Header">Nibh libero ornare pulvinar in metus.</h3>
+          <div className="About_Text_Body">
 
-          <p>Risus feugiat urna ut rhoncus. Euismod leo aliquam enim morbi tincidunt ultricies quam neque. Ultrices quisque viverra non nunc. Turpis sed ultrices habitant nibh arcu. Quis convallis sed.</p>
+            <p>Our goal is to help our customers find alternatives to traditional energy sources to power their systems with and help them reduce their contribution to earth’s greenhouse gas emissions. </p>
 
-          <ul className='About_List'>
-            <li>
-              <img src="" alt="" />
-              lorem
-            </li>
-            <li>
-              <img src="" alt="" />
-              lorem
-            </li>
-            <li>
-              <img src="" alt="" />
-              lorem
-            </li>
-          </ul>
+            <ul className='About_List'>
+              <li>
+                <img src={icon1} alt="" />
+                <p>
+                  We ensure 100% transparency
+                </p>
+                 
+              </li>
+              <li>
+                <img src={icon2} alt="" />
+                <p>
+                  We are equipped with a team of highly efficient and experienced personnels
+                </p>
+              </li>
+              <li>
+                <img src={icon3} alt="" />
+                <p>
+                  High level of precision in the execution of our services
+                </p>
+              </li>
+            </ul>
+          </div>
         </div>
 
       </div>
